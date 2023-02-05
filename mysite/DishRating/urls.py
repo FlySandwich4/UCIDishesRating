8e79f95e2e77a8c8b6e1b6df4fcc0713224910e4
Liam <liamwu4@gmail.com>
2023-02-05 03:17:04 -0800

    AlmostDone did:comments,urls,cata_htmls

--- mysite/DishRating/urls.py
+++ mysite/DishRating/urls.py
@@ -2,8 +2,16 @@ from django.urls import path
 
 from . import views
 
+app_name = 'DishRating'
+
 urlpatterns = [
-    path('', views.introPage, name='sth'),
+    path('', views.introPage, name='Home'),
     path('dishes',views.dishesPage, name='dishes Page'),
-    path('ember',views.emberPage, name="ember")
+    path('ember',views.emberPage, name="ember"),
+    path('dishes/<int:urlDishID>',views.commentPage, name="comment"),
+    path('grubb',views.grubbPage, name="grubb"),
+    path('pizza',views.pizzaPage, name="pizza"),
+    path('soups',views.soupsPage, name="soups"),
+    path('vegan',views.veganPage, name="vegan"),
+    path('bakery',views.bakeryPage, name="bakery"),
 ]
--- mysite/DishRating/views.py
+++ mysite/DishRating/views.py
@@ -26,3 +26,49 @@ def emberPage(request):
     return render(request, 'DishRating/Ember.html',{
         'dishData': dishData
     })
+
+
+def grubbPage(request):
+    dishData = Dishes.objects.filter(dishType="Grubb")
+    return render(request, 'DishRating/Grubb.html',{
+        'dishData': dishData
+    })
+
+def pizzaPage(request):
+    dishData = Dishes.objects.filter(dishType="Pizza")
+    return render(request, 'DishRating/Pizza.html',{
+        'dishData': dishData
+    })
+
+def soupsPage(request):
+    dishData = Dishes.objects.filter(dishType="Soups")
+    return render(request, 'DishRating/Soups.html',{
+        'dishData': dishData
+    })
+
+def veganPage(request):
+    dishData = Dishes.objects.filter(dishType="Vegan")
+    return render(request, 'DishRating/Vegan.html',{
+        'dishData': dishData
+    })
+
+def bakeryPage(request):
+    dishData = Dishes.objects.filter(dishType="Bakery")
+    return render(request, 'DishRating/Bakery.html',{
+        'dishData': dishData
+    })
+
+def commentPage(request, urlDishID):
+    dishData = Dishes.objects.get(dishID=urlDishID)
+    print(dishData)
+    commentData = Comments.objects.filter(dishID=urlDishID)
+    comUsrData = []
+    for each in commentData:
+        comUsrData.append([each,Users.objects.get(userAccount=each.userAccount)])
+    print("comment")
+    print(commentData)
+
+    return render(request,'DishRating/comment.html',{
+        'dishData':dishData,
+        'comUsrData':comUsrData,
+    })
